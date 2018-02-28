76fd4adf986b5bcb0a70b2b1fa8619987682cc3c
Sujay Jayakar <sujayakar@dropbox.com>
2018-02-28 13:42:47 -0800

    text hax

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -9,9 +9,12 @@ export class Cyclotron {
     private windowWidth;
     private windowHeight;
     private rectangles;
+    private text;
     private ticker;
-    private dirty;
+    private lanesDirty;
+    private lastViewport;
     private timeline;
+    private textOverlay;
     private bufferedMessages;
 
     constructor() {
@@ -34,7 +37,10 @@ export class Cyclotron {
         // TODO: Print that we're waiting for data or something here.
         var socket = new WebSocket("ws://127.0.0.1:3001", "cyclotron-ws");
         this.bufferedMessages = [];
+        var i = 0;
         socket.onmessage = event => {
+            // setTimeout(() => { this.addEvent(JSON.parse(event.data)); }, i++ * 10);
+            // this.bufferedMessages.push(JSON.parse(event.data));
             this.addEvent(JSON.parse(event.data));
         };
         socket.onopen = event => { socket.send("empty_file_release.log"); };
@@ -42,6 +48,7 @@ export class Cyclotron {
         socket.onclose = event => { alert(`Socket closed ${event}`); };
 
         this.rectangles = {};
+        this.text = {};
         this.timeline = new Viewport({
             screenWidth: this.windowWidth,
             screenHeight: this.windowHeight,
@@ -50,81 +57,186 @@ export class Cyclotron {
         });
         this.timeline.drag().wheel().decelerate();
         this.timeline.clamp({direction: "all"});
-
+        this.timeline.clampZoom({});
+        // Oh lord, monkey patch da zoom.
+        this.timeline.fitHeight = function (height, center) {
+            this.scale.y = this._screenHeight / height;
+            return this;
+        };
         this.app.stage.addChild(this.timeline);
+
+        this.textOverlay = new PIXI.Container();
+        this.textOverlay.x = 0;
+        this.textOverlay.y = 0;
+        this.textOverlay.width = this.windowWidth;
+        this.textOverlay.height = this.windowHeight;
+        this.app.stage.addChild(this.textOverlay);
 
         this.ticker = PIXI.ticker.shared;
         this.ticker.autoStart = true;
-        this.ticker.add(this.drawDemo, this);
+        this.ticker.add(this.draw, this);
 
-        this.dirty = false;
+        this.lanesDirty = false;
+        this.lastViewport = {width: 0, height: 0, ts: 0};
     }
 
     private addEvent(event) {
         this.spanManager.addEvent(event);
-        this.dirty = true;
+        this.lanesDirty = true;
     }
 
-    private drawDemo() {
-        if (!this.dirty) {
-            return;
-        }
-        this.dirty = false;
+    private viewportDirty() {
+        let viewArea = this.timeline.hitArea;
+        return this.lastViewport.width !== viewArea.width
+            || this.lastViewport.height !== viewArea.height
+            || this.lastViewport.ts !== viewArea.x;
+    }
 
-        this.timeline.x = 0;
-        this.timeline.y = 0;
+    private saveViewport() {
+        let viewArea = this.timeline.hitArea;
+        this.lastViewport = {
+            width: viewArea.width,
+            height: viewArea.height,
+            ts: viewArea.x
+        };
+    }
 
-        let maxHeight = this.spanManager.numLanes();
-        if (maxHeight === 0 || this.spanManager.maxTime === 0) {
-            return;
-        }
+    private draw() {
+        if (this.lanesDirty) {
+            this.lanesDirty = false;
 
-        this.timeline.worldWidth = this.spanManager.maxTime;
-        this.timeline.worldHeight = maxHeight;
-        this.timeline.scale.x = this.windowWidth / this.spanManager.maxTime;
-        this.timeline.scale.y = this.windowHeight / maxHeight;
+            let maxHeight = this.spanManager.numLanes();
+            if (maxHeight === 0 || this.spanManager.maxTime === 0) {
+                return;
+            }
 
-        let numDrawn = 0;
-        this.spanManager.listLanes().forEach(lane => {
-            lane.spans.forEach(span => {
-                let end = span.end ? span.end : this.spanManager.maxTime;
-                let container = this.rectangles[span.id];
-                if (container === undefined) {
-                    container = new PIXI.Container();
-                    let rect = new PIXI.Graphics();
-                    rect.beginFill(0x484848)
+            this.timeline.worldWidth = this.spanManager.maxTime;
+            this.timeline.worldHeight = maxHeight;
+
+            let clampZoom = this.timeline.plugins['clamp-zoom'];
+            clampZoom.minHeight = maxHeight;
+            clampZoom.maxHeight = maxHeight;
+            clampZoom.maxWidth = this.spanManager.maxTime;
+
+            let numDrawn = 0;
+            this.spanManager.listLanes().forEach(lane => {
+                lane.spans.forEach(span => {
+                    let end = span.end ? span.end : this.spanManager.maxTime;
+                    let rect = this.rectangles[span.id];
+                    if (rect === undefined) {
+                        rect = new PIXI.Graphics();
+                        this.timeline.addChild(rect);
+                        this.rectangles[span.id] = rect;
+                    }
+                    rect.clear();
+                    rect.beginFill(0x484848);
                     rect.drawRect(
-                        0,
-                        0,
+                        span.start,
+                        lane.index,
                         end - span.start,
                         0.9,
                     );
                     rect.endFill();
-                    container.addChild(rect);
 
-                    let style = new PIXI.TextStyle({fill: "white"});
-                    let name = new PIXI.Text(span.name, style);
-                    name.width = end - span.start;
-                    name.height = 0.9;
-                    container.addChild(name);
 
-                    this.rectangles[span.id] = container;
-                    this.timeline.addChild(container);
-                }
+                    //     container = new PIXI.Container();
+                    //     let rect = new PIXI.Graphics();
+                    //     rect.beginFill(0x484848)
+                    //     rect.drawRect(
+                    //         0,
+                    //         0,
+                    //         end - span.start,
+                    //         0.9,
+                    //     );
+                    //     rect.endFill();
+                    //     container.addChild(rect);
 
-                let rect = container.children[0];
-                rect.width = end - span.start;
-                rect.height = 0.9;
+                    //     // let style = new PIXI.TextStyle({fill: "white"});
+                    //     // let name = new PIXI.Text(span.name, style);
+                    //     // name.width = end - span.start;
+                    //     // name.height = 0.9;
+                    //     // container.addChild(name);
 
-                container.x = span.start;
-                container.y = lane.index;
-                container.width = end - span.start;
-                container.height = 0.9;
+                    //     this.rectangles[span.id] = container;
+                    //     this.timeline.addChild(container);
+                    // }
 
-                numDrawn += 1;
-            })
-        });
-        console.log(`Drew ${numDrawn} spans`);
+                    // let rect = container.children[0];
+                    // rect.width = end - span.start;
+                    // rect.height = 0.9;
+
+                    // container.x = span.start;
+                    // container.y = lane.index;
+                    // container.width = end - span.start;
+                    // container.height = 0.9;
+
+                    numDrawn += 1;
+                })
+            });
+            console.log(`Drew ${numDrawn} spans`);
+        }
+
+        if (this.viewportDirty()) {
+            let maxHeight = this.spanManager.numLanes();
+            if (maxHeight === 0 || this.spanManager.maxTime === 0) {
+                return;
+            }
+
+            let startTs = this.timeline.hitArea.x;
+            let endTs = startTs + this.timeline.hitArea.width;
+            let laneHeightPx = this.windowHeight / maxHeight;
+            let tsWidthPx = this.windowWidth / this.timeline.hitArea.width;
+
+            let numLabels = 0;
+            this.spanManager.listLanes().forEach(lane => {
+                lane.spans.forEach(span => {
+                    let visible = span.intersects(startTs, endTs);
+                    let text = this.text[span.id];
+                    if (text === undefined) {
+                        let style = new PIXI.TextStyle({fill: "white"});
+                        let name = span.name;
+                        if (name.length > 80) {
+                            name = name.substring(0, 77) + "...";
+                        }
+                        text = new PIXI.Text(name, style);
+                        this.text[span.id] = text;
+                        this.textOverlay.addChild(text);
+                    }
+                    text.visible = visible;
+                    text.mask = null;
+                    if (!visible) {
+                        return;
+                    }
+                    let scale = laneHeightPx / text.height;
+                    let screenRelTs = span.start - this.timeline.hitArea.x;
+                    if (screenRelTs < 0) {
+                        screenRelTs = 0;
+                    }
+                    let end = (span.end ? span.end : this.spanManager.maxTime)
+                        - this.timeline.hitArea.x;
+                    if (end > this.timeline.hitArea.width) {
+                        end = this.timeline.hitArea.width;
+                    }
+
+                    let widthTs = end - screenRelTs;
+
+                    text.x = screenRelTs * tsWidthPx;
+                    text.y = lane.index * laneHeightPx;
+                    text.height = text.height * scale;
+                    text.width = text.width * scale;
+
+                    if (text.width * scale > tsWidthPx * widthTs) {
+                        text.visible = false;
+                        return;
+                    }
+
+                    numLabels++;
+                });
+            });
+            console.log(`Drew ${numLabels} labels`);
+
+            this.saveViewport();
+        }
     }
 }
 
