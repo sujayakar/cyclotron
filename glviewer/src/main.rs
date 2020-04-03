9915c810dbf1738ce3b14a1eaa6f09f8a8090965
Joshua Warner <joshw@dropbox.com>
2020-04-02 19:58:22 -0700

    simplify rendering

--- glviewer/src/main.rs
+++ glviewer/src/main.rs
@@ -82,50 +82,30 @@ fn main() {
 
     #[derive(Copy, Clone)]
     struct Vertex {
-        position: [f32; 3],
-        normal: [f32; 3],
-        tex_coords: [f32; 2],
+        position: [f32; 2],
     }
 
-    implement_vertex!(Vertex, position, normal, tex_coords);
+    implement_vertex!(Vertex, position);
 
     let shape = glium::vertex::VertexBuffer::new(&display, &[
-            Vertex { position: [-1.0,  1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [0.0, 1.0] },
-            Vertex { position: [ 1.0,  1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [1.0, 1.0] },
-            Vertex { position: [-1.0, -1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [0.0, 0.0] },
-            Vertex { position: [ 1.0, -1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [1.0, 0.0] },
+            Vertex { position: [-1.0,  1.0] },
+            Vertex { position: [ 1.0,  1.0] },
+            Vertex { position: [-1.0, -1.0] },
+            Vertex { position: [ 1.0, -1.0] },
         ]).unwrap();
-
-
-    // let diffuse_texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
-    // let normal_map = glium::texture::Texture2d::new(&display, image).unwrap();
-
 
     let vertex_shader_src = r#"
         #version 150
-        in vec3 position;
-        in vec3 normal;
-        in vec2 tex_coords;
-        out vec3 v_normal;
-        out vec3 v_position;
-        out vec2 v_tex_coords;
-        uniform mat4 perspective;
-        uniform mat4 view;
-        uniform mat4 model;
+        in vec2 position;
+        uniform vec2 scale;
+        uniform vec2 offset;
         void main() {
-            v_tex_coords = tex_coords;
-            mat4 modelview = view * model;
-            v_normal = transpose(inverse(mat3(modelview))) * normal;
-            gl_Position = perspective * modelview * vec4(position, 1.0);
-            v_position = gl_Position.xyz / gl_Position.w;
+            gl_Position = vec4(position.xy*scale + offset, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
-        in vec3 v_normal;
-        in vec3 v_position;
-        in vec2 v_tex_coords;
         out vec4 color;
         void main() {
             color = vec4(1.0, 0.0, 0.0, 1.0);
@@ -164,32 +144,8 @@ fn main() {
         let mut target = display.draw();
         target.clear_color_and_depth((1.0, 1.0, 1.0, 1.0), 1.0);
 
-        let model = [
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0f32]
-        ];
-
-        let view = view_matrix(&[0.5, 0.2, -3.0], &[-0.5, -0.2, 3.0], &[0.0, 1.0, 0.0]);
-
-        let perspective = {
-            let (width, height) = target.get_dimensions();
-            let aspect_ratio = height as f32 / width as f32;
-
-            let fov: f32 = 3.141592 / 3.0;
-            let zfar = 1024.0;
-            let znear = 0.1;
-
-            let f = 1.0 / (fov / 2.0).tan();
-
-            [
-                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
-                [         0.0         ,     f ,              0.0              ,   0.0],
-                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
-                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
-            ]
-        };
+        let scale: [f32; 2] = [0.5, 0.5];
+        let offset: [f32; 2] = [0.0, 0.0];
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
@@ -201,42 +157,8 @@ fn main() {
         };
 
         target.draw(&shape, glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip), &program,
-                    &uniform! { model: model, view: view, perspective: perspective },
+                    &uniform! { scale: scale, offset: offset },
                     &params).unwrap();
         target.finish().unwrap();
     });
-}
-
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    let s = [up[1] * f[2] - up[2] * f[1],
-             up[2] * f[0] - up[0] * f[2],
-             up[0] * f[1] - up[1] * f[0]];
-
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
-             f[2] * s_norm[0] - f[0] * s_norm[2],
-             f[0] * s_norm[1] - f[1] * s_norm[0]];
-
-    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
 }
