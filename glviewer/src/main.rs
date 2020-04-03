fd4690f8e0a229b7b81f818ca09b9530b68ca937
Joshua Warner <joshw@dropbox.com>
2020-04-02 19:49:06 -0700

    wip, added parsing

--- glviewer/src/main.rs
+++ glviewer/src/main.rs
@@ -1,8 +1,80 @@
-use glium::{glutin, Surface, implement_vertex, uniform};
-use std::io::Cursor;
+use glium::{glutin, Surface, implement_vertex, uniform, index::PrimitiveType};
+use structopt::StructOpt;
+use cyclotron_backend::TraceEvent;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::collections::{HashMap, HashSet};
 
+#[derive(Debug, StructOpt)]
+struct Args {
+    trace: String,
+    // grep: Vec<String>,
+    // hide_wakeups: Vec<String>,
+}
 
 fn main() {
+    let args = Args::from_args();
+    
+    let mut file = BufReader::new(File::open(&args.trace).unwrap());
+    let mut events = Vec::new();
+
+    loop {
+        let mut buf = String::new();
+        let num_read = file.read_line(&mut buf).unwrap();
+
+        if num_read == 0 || !buf.ends_with("\n") {
+            break;
+        } else {
+            buf.pop();
+            let event: TraceEvent = serde_json::from_str(&buf).unwrap();
+            events.push(event);
+        }
+    }
+
+    let mut spans = HashMap::new();
+    let mut parents = HashSet::new();
+
+    for event in events {
+        match event {
+            TraceEvent::AsyncStart { id, ts, .. } |
+            TraceEvent::AsyncOnCPU { id, ts, .. } |
+            TraceEvent::SyncStart { id, ts, .. } |
+            TraceEvent::ThreadStart { id, ts, .. } => {
+                spans.entry(id).or_insert((None, None)).0 = Some(ts);
+            }
+            TraceEvent::AsyncOffCPU { id, ts, .. } |
+            TraceEvent::AsyncEnd { id, ts, .. } |
+            TraceEvent::SyncEnd { id, ts, .. } |
+            TraceEvent::ThreadEnd { id, ts, .. } => {
+                spans.entry(id).or_insert((None, None)).1 = Some(ts);
+            }
+
+            TraceEvent::Wakeup { .. } => {}
+        }
+
+        match event {
+            TraceEvent::AsyncStart { parent_id, .. } |
+            TraceEvent::SyncStart { parent_id, .. } => {
+                parents.insert(parent_id);
+            }
+
+            _ => {}
+        }
+    }
+
+    let mut spans = spans.into_iter().filter_map(|(k, v)| {
+        if parents.contains(&k) {
+            None
+        } else {
+            Some(((v.0).unwrap(), (v.1).unwrap()))
+        }
+    }).collect::<Vec<_>>();
+
+    spans.sort();
+
+    let min_time = spans[0].0;
+    let max_time = spans.iter().map(|a| a.1).max().unwrap();
+
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
@@ -81,7 +153,12 @@ fn main() {
                 glutin::event::StartCause::Init => (),
                 _ => return,
             },
-            _ => return,
+            glutin::event::Event::MainEventsCleared | 
+            glutin::event::Event::RedrawEventsCleared => return,
+            _ => {
+                // println!("{:?}", event);
+                return;
+            }
         }
 
         let mut target = display.draw();
