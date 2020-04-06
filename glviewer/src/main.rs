f4a429fd2b8452e547ef5e2043bd90e76b558b28
Joshua Warner <joshw@dropbox.com>
2020-04-06 10:20:39 -0700

    hide/show lanes based on whether they have content

--- glviewer/src/main.rs
+++ glviewer/src/main.rs
@@ -135,6 +135,8 @@ struct ViewBuilder {
     spans: HashMap<SpanId, TraceSpan>,
     assignments: HashMap<SpanId, LaneAssignment>,
     lanes: Vec<LaneBuilder>,
+    min_time: u64,
+    max_time: u64,
 }
 
 impl ViewBuilder {
@@ -143,11 +145,15 @@ impl ViewBuilder {
             spans: HashMap::new(),
             assignments: HashMap::new(),
             lanes: Vec::new(),
+            min_time: std::u64::MAX,
+            max_time: 0,
         }
     }
 
     fn add(&mut self, span: TraceSpan) {
         self.spans.insert(span.id, span.clone());
+        self.min_time = std::cmp::min(self.min_time, span.span.begin);
+        self.max_time = std::cmp::max(self.max_time, span.span.end);
 
         for (lane_id, lane) in self.lanes.iter_mut().enumerate() {
             if let Some(index) = lane.try_add(span.id, span.span) {
@@ -163,9 +169,19 @@ impl ViewBuilder {
     }
 
     fn build(self, display: &Display) -> View {
+        let now = Instant::now();
         View {
+            scale_offset: ScaleOffset::new(
+                (self.min_time as f32) / 1_000_000_000.0,
+                (self.max_time as f32) / 1_000_000_000.0),
             spans: self.spans,
             plain_rect: build_rect(display),
+            lane_sizes: self.lanes.iter().map(|_| LaneSize {
+                target: TargetSize::Full,
+                old_size: 0.0,
+                begin: now,
+                end: now
+            }).collect(),
             lanes: self.lanes.into_iter().map(|l| l.build(display)).collect(),
             highlight_lane: None,
         }
@@ -181,11 +197,33 @@ fn build_rect(display: &Display) -> VertexBuffer<Vertex> {
     ]).unwrap()
 }
 
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum TargetSize {
+    Full,
+    Hidden,
+}
+
+struct LaneSize {
+    target: TargetSize,
+    old_size: f32,
+    begin: Instant,
+    end: Instant,
+}
+
 struct View {
+    scale_offset: ScaleOffset,
     spans: HashMap<SpanId, TraceSpan>,
     plain_rect: VertexBuffer<Vertex>,
     lanes: Vec<Lane>,
+    lane_sizes: Vec<LaneSize>,
     highlight_lane: Option<(usize, Option<usize>)>,
+}
+
+struct LaneRect {
+    offset: f32,
+    scale: f32,
+    min: f32,
+    max: f32,
 }
 
 impl View {
@@ -198,18 +236,70 @@ impl View {
         b.build(display)
     }
 
-    fn hover(&mut self, pixel_coord: (i32, i32), display: (i32, i32), scale_offset: &ScaleOffset) {
-        let scale = scale_offset.scale();
-        let offset = scale_offset.offset();
+    fn compute_lane_rects(&self) -> Vec<LaneRect> {
+        let now = Instant::now();
+
+        let actual_sizes = self.lane_sizes.iter().map(|size| {
+            if now < size.begin {
+                match size.target {
+                    TargetSize::Full => 0.0,
+                    TargetSize::Hidden => 1.0,
+                }
+            } else if now > size.end {
+                match size.target {
+                    TargetSize::Full => 1.0,
+                    TargetSize::Hidden => 0.0,
+                }
+            } else {
+                match size.target {
+                    TargetSize::Full => 1.0,
+                    TargetSize::Hidden => 0.0,
+                }
+
+                // XXX: this animation was making me sick.
+                // let frac = (size.end - size.begin).as_secs_f32() / (now - size.begin).as_secs_f32();
+                // match size.target {
+                //     TargetSize::Full => frac,
+                //     TargetSize::Hidden => 1.0 - frac,
+                // }
+            }
+        }).collect::<Vec<_>>();
+
+        let mut cum_sums = Vec::new();
+
+        let mut total_size = 0.0;
+        for size in &actual_sizes {
+            cum_sums.push(total_size);
+            total_size += size;
+        }
+
+        let mut res = Vec::new();
+
+        for (size, cum_sum) in actual_sizes.iter().zip(cum_sums.iter()) {
+            res.push(LaneRect {
+                offset: *cum_sum,
+                scale: size / total_size,
+                min: cum_sum / total_size,
+                max: (cum_sum + size) / total_size,
+            });
+        }
+
+        res
+    }
+
+    fn hover(&mut self, pixel_coord: (i32, i32), display: (i32, i32)) {
+        let scale = self.scale_offset.scale();
+        let offset = self.scale_offset.offset();
 
         let old_highlight = self.highlight_lane;
 
         self.highlight_lane = None;
 
-        let lanes_len = self.lanes.len();
+        let rects = self.compute_lane_rects();
+
         for (i, lane) in self.lanes.iter_mut().enumerate() {
-            let pixel_begin = (i + 1) as f32 / (lanes_len + 2) as f32 * display.1 as f32;
-            let pixel_end = (i + 2) as f32 / (lanes_len + 2) as f32 * display.1 as f32;
+            let pixel_begin = rects[i].min * display.1 as f32;
+            let pixel_end = rects[i].max * display.1 as f32;
 
             if pixel_coord.1 as f32 >= pixel_begin && (pixel_coord.1 as f32) < pixel_end {
                 let index = lane.hover(pixel_coord.0, display.0, scale, offset);
@@ -242,9 +332,9 @@ impl View {
         }
     }
 
-    fn draw(&self, program: &glium::Program, target: &mut Frame, scale_offset: &ScaleOffset) {
-        let scale = scale_offset.scale();
-        let offset = scale_offset.offset();
+    fn draw(&self, program: &glium::Program, target: &mut Frame) {
+        let scale = self.scale_offset.scale();
+        let offset = self.scale_offset.offset();
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
@@ -255,11 +345,13 @@ impl View {
             .. Default::default()
         };
 
+        let rects = self.compute_lane_rects();
+
         for (i, lane) in self.lanes.iter().enumerate() {
             let color = hsl_to_rgb(i as f32 / self.lanes.len() as f32, 0.5, 0.2);
 
-            let vert_scale = 1.0 / (self.lanes.len() + 2) as f32;
-            let vert_offset = (i + 1) as f32;
+            let vert_offset = rects[i].offset;
+            let vert_scale = rects[i].scale;
 
             if let Some((lane_id, _)) = self.highlight_lane {
                 if i == lane_id {
@@ -286,6 +378,40 @@ impl View {
                                 &uniform! { scale: scale_vec, offset: offset_vec, item_color: [1.0f32, 0.0, 0.0, 1.0] },
                                 &params).unwrap();
                 }
+            }
+        }
+    }
+
+    fn scroll(&mut self, offset_scroll: f32, scale_scroll: f32, ptr_loc: f32) {
+        self.scale_offset.scroll(offset_scroll, scale_scroll, ptr_loc);
+        
+        let scale = self.scale_offset.scale();
+        let offset = self.scale_offset.offset();
+
+        // 0 = (left + offset) * scale
+        let left = - offset;
+
+        // 1 = (right + offset) * scale
+        let right = 1.0/scale - offset;
+
+        let left = (left * 1_000_000_000.0) as u64;
+        let right = (right * 1_000_000_000.0) as u64;
+
+        let now = Instant::now();
+
+        let mut i = 0;
+        for (lane, size) in self.lanes.iter().zip(self.lane_sizes.iter_mut()) {
+            i += 1;
+            let target = if lane.has_contents(left, right) {
+                TargetSize::Full
+            } else {
+                TargetSize::Hidden
+            };
+
+            if target != size.target {
+                size.target = target;
+                size.begin = now;
+                size.end = now + Duration::from_millis(1000);
             }
         }
     }
@@ -321,6 +447,13 @@ impl Lane {
             vertex,
             index,
         }
+    }
+
+    fn has_contents(&self, left: u64, right: u64) -> bool {
+        let min = self.spans.binary_search_by_key(&left, |s| s.end).unwrap_or_else(|e| e);
+        let max = self.spans.binary_search_by_key(&right, |s| s.begin).unwrap_or_else(|e| e);
+
+        min < max
     }
 
     fn hover(&mut self, pixel_coord: i32, display: i32, scale: f32, offset: f32) -> Option<usize> {
@@ -515,7 +648,7 @@ fn main() {
                 },
                 glutin::event::WindowEvent::CursorMoved { position, .. } => {
                     let dims = display.get_framebuffer_dimensions();
-                    view.hover((position.x as i32, position.y as i32), (dims.0 as i32, dims.1 as i32), &scale_offset);
+                    view.hover((position.x as i32, position.y as i32), (dims.0 as i32, dims.1 as i32));
                     ptr_loc = position.x as f32 / dims.0 as f32;
                 }
                 _ => return,
@@ -530,7 +663,7 @@ fn main() {
             glutin::event::Event::DeviceEvent { event, .. } => match event {
                 glutin::event::DeviceEvent::MouseWheel { delta: 
                     glutin::event::MouseScrollDelta::PixelDelta(delta) } => {
-                    scale_offset.scroll(delta.x as f32, delta.y as f32, ptr_loc);
+                    view.scroll(delta.x as f32, delta.y as f32, ptr_loc);
                 }
                 _ => {}
             },
@@ -544,7 +677,7 @@ fn main() {
         let mut target = display.draw();
         target.clear_color_and_depth((1.0, 1.0, 1.0, 1.0), 1.0);
 
-        view.draw(&program, &mut target, &scale_offset);
+        view.draw(&program, &mut target);
 
         // target.draw(&lane.vertex, &lane.index, &program,
         //             &uniform! { scale: scale_vec, offset: offset_vec, item_color: [0.0f32, 0.0, 0.0, 1.0] },
